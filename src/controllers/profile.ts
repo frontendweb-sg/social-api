58492358083d8887585a45acf72db26cdab50b80
frontendweb-sg <frontendweb.sg@gmail.com>
2024-04-09 18:42:50 +0530

    update profile

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,23 +1,98 @@
-import {NextFunction, Request, Response} from 'express';
-import {IUser, User} from '../models/user';
-import {Post} from '../models/post';
+import {Request, Response, NextFunction} from 'express';
+import {IProfileDoc, Profile} from '../models/profile';
+import {BadRequestError} from '../errors';
 
-/**
- * Get logged in user posts
- * @param req
- * @param res
- * @param next
- * @returns
- */
-export const getLoggedInUserPosts = async (
+export const getProfile = async (
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ) => {
 	try {
 		const user = req.user?.id;
-		const posts = await Post.find({user});
-		return res.status(200).json(posts);
+		const profile = (await Profile.findOne({user})) as IProfileDoc;
+
+		return res.status(200).json(profile);
+	} catch (error) {}
+};
+
+/**
+ * Create user profile
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+export const addProfile = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const user = req.user?.id;
+		const body = req.body;
+		body.user = user;
+
+		const profile = await Profile.findOne({
+			user,
+		});
+
+		if (profile) {
+			throw new BadRequestError('Profile already existed, please update');
+		}
+
+		const newProfile = new Profile(body);
+		const result = await newProfile.save();
+		return res.status(201).json(result);
+	} catch (error) {
+		console.log('E', error);
+		next(error);
+	}
+};
+
+/**
+ * Update user profile
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+export const updateProfile = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const user = req.user?.id;
+		const profileId = req.params.profileId;
+		const body = req.body;
+
+		const profile = await Profile.findOne({
+			user,
+		});
+
+		if (!profile)
+			throw new BadRequestError('Profile not existed!, pelase create');
+
+		const result = await Profile.findOneAndUpdate(
+			{user, _id: profileId},
+			{$set: body},
+			{new: true},
+		);
+		return res.status(200).json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getGithubProfile = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const user = req.user?.id;
+		const profileId = req.params.profileId;
+		const body = req.body;
 	} catch (error) {
 		next(error);
 	}
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,8 +1,31 @@
 import {Router} from 'express';
-import {getLoggedInUserPosts} from '../controllers/profile';
 import {auth} from '../middleware/auth';
+import {addProfile, getProfile, updateProfile} from '../controllers/profile';
+import {
+	addEducation,
+	deleteEducation,
+	updateEducation,
+} from '../controllers/education';
+import {
+	addEmployment,
+	updateEmployment,
+	deleteEmployment,
+} from '../controllers/employment';
 
 const route = Router();
-route.get('/:userId/posts', auth, getLoggedInUserPosts);
+
+route.get('/', auth, getProfile);
+route.post('/', auth, addProfile);
+route.put('/:profileId', auth, updateProfile);
+
+// employment route
+route.post('/:profileId/employment', auth, addEmployment);
+route.put('/:profileId/employment/:employmentId', auth, updateEmployment);
+route.delete('/:profileId/employment/:employmentId', auth, deleteEmployment);
+
+// education route
+route.post('/:profileId/education', auth, addEducation);
+route.put('/:profileId/education/:educationId', auth, updateEducation);
+route.delete('/:profileId/education/:educationId', auth, deleteEducation);
 
 export {route as profileRoute};
